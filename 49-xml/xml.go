c7ed419d7d935049aafab6411bdf2b1e33544a01
Anaet Hossain <anaethoss@gmail.com>
2019-12-31 13:19:07 +0600

    time

--- 49-xml/xml.go
+++ 49-xml/xml.go
@@ -5,19 +5,20 @@ import (
 	"fmt"
 )
 
+// Plant struct for checking xml
 type Plant struct {
 	XMLName xml.Name `xml:"plant"`
-	Id      int      `xml:"id,attr"`
+	ID      int      `xml:"id,attr"`
 	Name    string   `xml:"name"`
 	Origin  []string `xml:"origin"`
 }
 
 func (p Plant) String() string {
-	return fmt.Sprintf("Plant id=%v, name=%v, origin=%v", p.Id, p.Name, p.Origin)
+	return fmt.Sprintf("Plant id=%v, name=%v, origin=%v", p.ID, p.Name, p.Origin)
 }
 
 func main() {
-	coffee := &Plant{Id: 27, Name: "Coffee"}
+	coffee := &Plant{ID: 27, Name: "Coffee"}
 	coffee.Origin = []string{"Ethiopia", "Brazil"}
 
 	out, _ := xml.MarshalIndent(coffee, " ", "  ")
@@ -31,7 +32,7 @@ func main() {
 	}
 	fmt.Println(p)
 
-	tomato := &Plant{Id: 81, Name: "Tomato"}
+	tomato := &Plant{ID: 81, Name: "Tomato"}
 	tomato.Origin = []string{"Mexico", "California"}
 
 	type Nesting struct {
